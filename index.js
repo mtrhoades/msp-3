b6c9aba69732878095526ad0a7a3c9f539212475
root <mtrhoades0727@gmail.com>
2022-07-31 13:22:16 -1000

    connecting to heroku

--- index.js
+++ index.js
@@ -1,17 +1,26 @@
 // dependencies
 const express = require('express');
 const cors = require('cors');
+const path = require('path');
 
 // config
 const app = express();
 require('dotenv').config();
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 3070
 
 
 // middleware
 app.use(cors());
 app.use(express.json()); //req.body on client side
 
+// app.use(express.static(path.join(__dirname, 'client/build')));
+
+if (process.env.NODE_ENV === "production") {
+    app.use(express.static(path.join(__dirname, 'client/build')));
+}
+
+console.log(__dirname);
+console.log(path.join(__dirname, 'client/build'))
 
 // root route
 app.get('/', (req, res) => {
@@ -22,6 +31,12 @@ app.get('/', (req, res) => {
 // controller routes
     // resister and login routes
 app.use('/auth', require('./controllers/jwtAuth'));
+
+
+// catch all route
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, "client/build/index.html"))
+})
 
 // server listen
 app.listen(PORT, () => {
--- models/db.js
+++ models/db.js
@@ -1,13 +1,38 @@
 
 const Pool = require('pg').Pool;
+require('dotenv').config();
 
-const pool = new Pool ({
-    host: "ec2-34-193-44-192.compute-1.amazonaws.com",
-    user: "wcbkbmkoxzutux",
-    password: "9b18493d40cef8edbd02e482a62a01e551e60af97613b4d4379bfccd14087c5a",
-    port: 5432,
-    database: "dfl7shtsg0hv9q"
+
+// const devConfig = {
+//         user: process.env.PG_USER,
+//         password: process.env.PG_PASSWORD,
+//         host: process.env.PG_HOST,
+//         database: process.env.PG_DATABASE,
+//         port: process.env.PG_PORT
+// }
+
+const devConfig = `postgresql://${process.env.PG_USER}:${process.env.PG_PASSWORD}@${process.env.HOST}:${process.env.PG_PORT}/${process.env.PG_DATABASE}`;
+
+const proConfig = process.env.DATABASE_URL // heroku addons
+
+const pool = new Pool({
+    connectionString:
+        process.env.NODE_ENV === 'production' ? proConfig : devConfig
 });
+
+
+
+
+
+// const pool = new Pool ({
+//     host: "ec2-34-193-44-192.compute-1.amazonaws.com",
+//     user: "wcbkbmkoxzutux",
+//     password: "9b18493d40cef8edbd02e482a62a01e551e60af97613b4d4379bfccd14087c5a",
+//     port: 5432,
+//     database: "dfl7shtsg0hv9q"
+// });
+
+
 
 
 
